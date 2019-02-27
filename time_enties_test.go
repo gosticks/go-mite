ca7ee35e17fa621f3f1f858754f28db416ae0991
Wlad Meixner <wlad.meixner@protonmail.com>
2019-02-27 22:59:54 +0100

    create delete test

--- time_enties_test.go
+++ time_enties_test.go
@@ -3,6 +3,7 @@ package mite_test
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/jinzhu/now"
 
@@ -33,4 +34,34 @@ func TestGetTimeEntriesGroup(t *testing.T) {
 	}
 
 	t.Logf("loaded %d groups", len(entries))
+}
+
+func TestCreateDeleteTimeEntry(t *testing.T) {
+	username, okUser := os.LookupEnv("MITE_USER")
+	team, okAddr := os.LookupEnv("MITE_TEAM")
+	key, okKey := os.LookupEnv("MITE_APIKEY")
+	if !okAddr || !okUser || !okKey {
+		t.Errorf("username=%s, team=%s and key=%s are required", username, team, key)
+		t.FailNow()
+	}
+
+	m := mite.NewMiteAPI(username, team, key, "test@go-mite")
+
+	newEntry := &mite.TimeEntryCreator{
+		DateAt:  mite.Time{time.Now()},
+		Minutes: 60,
+		Note:    "TEST NOTE CREATED BY GO-MITE API. PLEASE REMOVE",
+	}
+
+	entry, errEntry := m.CreateTimeEntry(newEntry)
+	if errEntry != nil {
+		t.Error(errEntry)
+	}
+
+	t.Logf("Created entry: %d", entry.ID)
+
+	errDel := m.DeleteTimeEntry(entry.ID)
+	if errDel != nil {
+		t.Error(errDel)
+	}
 }
